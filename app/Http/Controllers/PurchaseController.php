864b2f3dc35e7951147f99b52f73052cf1d69c3b
kazuya0515 <count4my8beat0121@yahoo.co.jp>
2023-02-28 23:16:43 +0900

    sync

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -9,7 +9,8 @@ use Inertia\Inertia;
 use App\Models\Customer;
 use App\Models\Item;
 use Illuminate\Support\Facades\DB;
-
+use App\Models\Order;
+use JetBrains\PhpStorm\Pure;
 
 class PurchaseController extends Controller
 {
@@ -20,7 +21,18 @@ class PurchaseController extends Controller
 	 */
 	public function index()
 	{
-		//
+		// dd(Order::paginate(50));
+
+		$orders = Order::groupBy('id')
+			->selectRaw('id, sum(subtotal) as total,
+		customer_name, status, created_at')
+			->paginate(50);
+
+		// dd($orders);
+
+		return Inertia::render('Purchases/Index', [
+			'orders' => $orders
+		]);
 	}
 
 	/**
@@ -49,33 +61,29 @@ class PurchaseController extends Controller
 	 */
 	public function store(StorePurchaseRequest $request)
 	{
-		
+
 		DB::beginTransaction();
 
-		try{
+		try {
 			$purchase = Purchase::create([
 				'customer_id' => $request->customer_id,
 				'status' => $request->status
 			]);
-	
-			foreach($request->items as $item){
+
+			foreach ($request->items as $item) {
 				$purchase->items()->attach($purchase->id, [
 					'item_id' => $item['id'],
 					'quantity' => $item['quantity']
 				]);
-			} 
+			}
 			DB::commit();
 
 			return to_route('dashboard');
-		}
-
-		catch(\Exception $e){
+		} catch (\Exception $e) {
 			DB::rollBack();
 		}
-		
-		
-		}
-	
+	}
+
 
 	/**
 	 * Display the specified resource.
@@ -85,7 +93,22 @@ class PurchaseController extends Controller
 	 */
 	public function show(Purchase $purchase)
 	{
-		//
+		// 小計
+		$items = Order::where('id', $purchase->id)->get();
+
+		// 合計
+		$order = Order::groupBy('id')
+			->where('id', $purchase->id)
+			->selectRaw('id, sum(subtotal) as total,
+		customer_name, status, created_at')
+			->get();
+
+		// dd($items, $order);
+
+		return Inertia::render('Purchases/Show', [
+			'items' => $items,
+			'order' => $order
+		]);
 	}
 
 	/**
@@ -96,7 +119,41 @@ class PurchaseController extends Controller
 	 */
 	public function edit(Purchase $purchase)
 	{
-		//
+		$purchase = Purchase::find($purchase->id);
+
+		$allItems = Item::select('id', 'name', 'price')
+			->get();
+
+		$items = [];
+
+		foreach ($allItems as $allItem) {
+			$quantity = 0;
+			foreach ($purchase->items as $item) {
+				if ($allItem->id === $item->id) {
+					$quantity = $item->pivot->quantity;
+				}
+			}
+			array_push($items, [
+				'id' => $allItem->id,
+				'name' => $allItem->name,
+				'price' => $allItem->price,
+				'quantity' => $quantity
+
+			]);
+		}
+
+		// dd($items);
+
+		$order = Order::groupBy('id')
+			->where('id', $purchase->id)
+			->selectRaw('id, customer_id,
+		customer_name, status, created_at')
+			->get();
+
+		return Inertia::render('Purchases/Edit', [
+			'items' => $items,
+			'order' => $order
+		]);
 	}
 
 	/**
@@ -108,7 +165,31 @@ class PurchaseController extends Controller
 	 */
 	public function update(UpdatePurchaseRequest $request, Purchase $purchase)
 	{
-		//
+		DB::beginTransaction();
+
+		try {
+		
+		$purchase->status = $request->status;
+		$purchase->save();
+
+		$items = [];
+
+		foreach ($request->items as $item) {
+			$items = $items + [
+				$item['id'] => [
+					'quantity' => $item['quantity']
+				]
+			];
+		}
+		
+		$purchase->items()->sync($items);
+
+		DB::commit();
+
+		return to_route(('dashboard'));
+	} catch (\Exception $e) {
+		DB::rollBack();
+	}
 	}
 
 	/**
